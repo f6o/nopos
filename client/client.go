d3b08b855a6719281a9868fb33ad510ba5249672
f6o <25875630+f6o@users.noreply.github.com>
2025-02-11 22:19:23 +0900

    check freq

--- client/client.go
+++ client/client.go
@@ -2,12 +2,35 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"sort"
 
 	"github.com/f6o/napos/hands"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
+
+func getCard(client hands.DealerClient) *hands.Card {
+	card, err := client.DealRandomCard(context.Background(), &hands.DealRandomCardRequest{})
+	if err != nil {
+		log.Fatalf("could not deal card: %v", err)
+	}
+	return card
+}
+
+func getUniqueCards(client hands.DealerClient) map[string]bool {
+	dealtCards := make(map[string]bool)
+	for {
+		card := getCard(client)
+		cardKey := fmt.Sprintf("%v-%d", card.Suit, card.Rank)
+		if dealtCards[cardKey] {
+			break
+		}
+		dealtCards[cardKey] = true
+	}
+	return dealtCards
+}
 
 func main() {
 	opt := grpc.WithTransportCredentials(insecure.NewCredentials())
@@ -19,13 +42,20 @@ func main() {
 
 	client := hands.NewDealerClient(conn)
 
-	// Deal a card
-	// req := &hands.DealRequest{Seed: 12345}
-	// card, err := client.DealCard(context.Background(), req)
-	card, err := client.DealRandomCard(context.Background(), &hands.DealRandomCardRequest{})
-	if err != nil {
-		log.Fatalf("could not deal card: %v", err)
+	frequency := make(map[int]int)
+	for i := 0; i < 100; i++ {
+		dealtCards := getUniqueCards(client)
+		length := len(dealtCards)
+		frequency[length]++
 	}
 
-	log.Printf("Dealt card: %v of rank %d", card.Suit, card.Rank)
+	log.Println("Frequency distribution of unique card lengths:")
+	lengths := make([]int, 0, len(frequency))
+	for length := range frequency {
+		lengths = append(lengths, length)
+	}
+	sort.Ints(lengths)
+	for _, length := range lengths {
+		log.Printf("Length %d: %d times", length, frequency[length])
+	}
 }
